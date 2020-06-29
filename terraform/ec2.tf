9f797f654b5767cbbe8e199911be0b7b28ba7f93
Ryan Quiz <974958+rquiz@users.noreply.github.com>
2020-06-29 11:27:38 -0700

    Fix EC2 declaration - prevents specific SG entry

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "webserver_a" {
     ami                         = data.aws_ami.ubuntu.id
     instance_type               = "t2.micro"
     associate_public_ip_address = true
-    vpc_security_group_ids      = [aws_security_group.sg_ingress_a.id]
+    #vpc_security_group_ids      = [aws_security_group.sg_ingress_a.id.[0]]
 
     tags = {
         name        = "webserver_a"
