97710baca7ef5d5a0d262c256155b79c0a360723
Ryan Quiz <974958+rquiz@users.noreply.github.com>
2020-06-29 11:19:05 -0700

    Adds VPC networking + tagged resources + ec2 test instance

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,31 @@
 # EC2.tf
 # define  compute instances
 
-resource "ec2" {
-    
+# Ensure loading Ubuntu AMI from canonical
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
+
+resource "aws_instance" "webserver_a" {
+    ami                         = data.aws_ami.ubuntu.id
+    instance_type               = "t2.micro"
+    associate_public_ip_address = true
+    vpc_security_group_ids      = [aws_security_group.sg_ingress_a.id]
+
+    tags = {
+        name        = "webserver_a"
+        realm       = "sitea"
+        trash_level = "high"
+    }
+}
+
--- terraform/main.tf
+++ terraform/main.tf
@@ -13,51 +13,79 @@
 provider "aws" {
     region                  = "us-west-2"
     shared_credentials_file = "/Users/ryan/.aws/credentials"
-    profile                 = "me"
+    profile                 = "terraform"
 }
 
 # define basic network settings for isolated testing
 ############
 
-resource "aws_vpc" "example" {
-    cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "vpc_a" {
+    cidr_block  = "10.0.0.0/16"
+    tags        = {
+        realm       = "sitea"
+        trash_level = "low"
+    }
 }
 
-resource "aws_internet_gateway" "gateway" {
-    vpc_id = "${aws_vpc.vpc.id}"
+resource "aws_internet_gateway" "gateway_a" {
+    vpc_id  = aws_vpc.vpc_a.id
+    tags    = {
+        realm       = "sitea"
+        trash_level = "low"
+    }
 }
 
-resource "aws_route" "route" {
-    route_table_id         = "${aws_vpc.vpc.main_route_table_id}"
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id             = "${aws_internet_gateway.gateway.id}
+resource "aws_route" "route_a" {
+    route_table_id          = aws_vpc.vpc_a.main_route_table_id
+    destination_cidr_block  = "0.0.0.0/0"
+    gateway_id              = aws_internet_gateway.gateway_a.id
 }
- 
+
 data "aws_availability_zones" "available" {}
  
-resource "aws_subnet" "main" {
-    count                   = "${length(data.aws_availability_zones.available.names)}"
-    vpc_id                  = "${aws_vpc.vpc.id}"
+resource "aws_subnet" "subnet_a" {
+    count                   = length(data.aws_availability_zones.available.names)
+    vpc_id                  = aws_vpc.vpc_a.id
     cidr_block              = "10.0.${count.index}.0/24"
     map_public_ip_on_launch = false
-    availability_zone       = "${element(data.aws_availability_zones.available.names, count.index)}"
+    availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+    tags                    = {
+        realm       = "sitea"
+        trash_level = "low"
+    }
 }
 
-resource "aws_security_group" "default" {
+resource "aws_security_group" "sg_ingress_a" {
     name        = "http-https-allow"
     description = "Allow incoming HTTP and HTTPS and Connections"
-    vpc_id      = "${aws_vpc.vpc.id}"
+    vpc_id      = aws_vpc.vpc_a.id
 
     ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
+        description = "TLS from within VPC"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = [aws_vpc.vpc_a.cidr_block]
+    }
+    
+    ingress {
+        description = "HTTP from world"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags        = {
+        realm       = "sitea"
+        trash_level = "medium"
     }
 }
+
